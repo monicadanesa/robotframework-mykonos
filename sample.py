a2e378a3a59541a390bae95401f062e25c96801c
Akhmad Andriana Khadafi <akhmad.andriana@kudo.co.id>
2019-05-03 23:53:45 +0700

    [element] add get coordinate element by x & y

--- sample.py
+++ sample.py
@@ -4,3 +4,39 @@ from mykonos.keywords.element import Element
 from mykonos.keywords.touch import Touch
 from mykonos.locator.locator_element import LocatorElement
 from mykonos.core.core import Core
+
+el = Element()
+sample = el.get_element_by_coordinate_y(text='All contacts')
+
+sample
+test = el.get_element_by_coordinate_x(text='All contacts')
+
+test
+
+from uiautomator import Device
+
+d = Device()
+tc  = d(text='All contacts').info['bounds']
+
+width = d.info['displayWidth']
+height = d.info['displayHeight']
+bottom = d(text='All contacts').info['bounds']['bottom']
+right = d(text='All contacts').info['bounds']['right']
+left = d(text='All contacts').info['bounds']['left']
+top = d(text='All contacts').info['bounds']['top']
+
+d
+
+top
+width
+height
+left
+
+right
+bottom
+
+elm_y = height-(right+left)
+elm_x = (top+bottom)+top
+
+elm_x
+elm_y
