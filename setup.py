a2f5f1dde1f3f2fd3369da0c8bb36800a3cc08af
monica danesa <monicadanesa@gmail.com>
2019-06-08 01:21:37 +0700

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='mykonos',
     version='0.0.1',
     description='Robot Framework Mobile Test Automation using UIAutomator',
+    url='https://github.com/monicadanesa/mykonos',
+    author='Monica Danesa and Ahmad Kadafi',
+    author_email='monicadanesa@gmail.com, andriana.khadafi@gmail.com',
+    keywords='Robot Framework Mobile Test Automation using UIAutomator',
     packages=['mykonos'],
     install_requires=[
-        'uiautomator', 'robotframework'
+        'robotframework', 'uiautomator'
     ],
-    entry_points={'console_scripts': ['mykonos = mykonos.__init__:mykonos']})
+    test_requires=[
+        'pyyaml', 'pytest', 'pytest-html', 'pytest-mock', 'mock'
+    ])
