4c01143d76c2a24ddcf87b064991fb21b6842f6b
Monica Danesa <monicadanesa@users.noreply.github.com>
2019-06-08 10:14:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     keywords='Robot Framework Mobile Test Automation using UIAutomator',
     packages=find_packages(exclude=["demo", "docs", "tests", ]),
     install_requires=[
-        'robotframework', 'uiautomator', 'alog', 'selenium', 'imgUtil', 'comparision', 'chromedriver'
+        'robotframework', 'uiautomator', 'alog'
     ],
     test_requires=[
         'pyyaml', 'pytest', 'pytest-html', 'pytest-mock', 'mock'
